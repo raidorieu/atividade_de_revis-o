a5e8297118bd3a8e081b450056e42d41d45b2d31
Bernardo Guerra <bernardoguerra93@gmail.com>
2023-09-28 11:54:20 -0300

    exercicio

--- exercicio_12.py
+++ exercicio_12.py
@@ -1,1 +1,4 @@
-eleitorestotal=float(input("quantas pessoas votaram? "))
+eleitoresbranco=int(input("quantas pessoas votaram em branco? "))
+eleitoresnulo=int(input("quantas pessoas votaram nulo? "))
+eleitoresvalidos=int(input("quantas pessoas votaram válidos? "))
+
