2031da7d50c14ef72a4502e42cbb57549285b283
Bernardo Guerra, Raidori <143803158+raidorieu@users.noreply.github.com>
2023-09-28 14:38:33 -0300

    Delete exercicio_12.py

--- exercicio_12.py
+++
@@ -1,4 +0,0 @@
-eleitoresbranco=int(input("quantas pessoas votaram em branco? "))
-eleitoresnulo=int(input("quantas pessoas votaram nulo? "))
-eleitoresvalidos=int(input("quantas pessoas votaram válidos? "))
-
