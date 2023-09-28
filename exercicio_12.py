8b6bfbd8e7006c6b32a2883b02a7a9dc97b9e6bb
Bernardo Guerra, Raidori <143803158+raidorieu@users.noreply.github.com>
2023-09-28 14:45:13 -0300

    Add files via upload

---
+++ exercicio_12.py
@@ -0,0 +1,3 @@
+eleitoresbranco=int(input("quantas pessoas votaram em branco? "))
+eleitoresnulo=int(input("quantas pessoas votaram nulo? "))
+eleitoresvalidos=int(input("quantas pessoas votaram válidos? "))
